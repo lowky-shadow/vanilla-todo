917882e431b7eb7953bc670fcb2ab8b42cc73a1a
lowky-shadow <share99anyone2@gmail.com>
2026-01-03 21:43:25 +0530

    adding a input box

--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
-console.log("app started")
-let arr =[];
-let a = document.querySelector("#todo-selector");
+const main = async () => {
+  console.log("app started");
+  let arr = [];
+  let a = document.querySelector("#todo-selector");
 
-arr.push("mango bring");
-arr.push("bring apple");
+  arr.push("mango bring");
+  arr.push("bring apple");
 
-for(let i = 0; i < arr.length; i++){
-  let li = document.createElement("li");
-  li.textContent = arr[i];
-  a.appendChild(li);
+  const form = document.querySelector("#form");
 
-}
+   form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // const input = document.querySelector("#input_todo");
+    const input = e.target.todo.value;
+    arr.push(input);
+  });
+  console.log(arr);
+
+  for (let i = 0; i < arr.length; i++) {
+    let li = document.createElement("li");
+    li.textContent = arr[i];
+    a.appendChild(li);
+  }
+};
+main();
