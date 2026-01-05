cfb983888638c8e214ea9a6147b2c47b49c873e2
lowky-shadow <share99anyone2@gmail.com>
2026-01-05 18:50:08 +0530

    add and delete todo

--- src/main.js
+++ src/main.js
@@ -1,25 +1,48 @@
 const main = async () => {
-  console.log("app started");
-  let arr = [];
-  let a = document.querySelector("#todo-selector");
+  let j = 0;
+  let todos = [];
 
-  arr.push("mango bring");
-  arr.push("bring apple");
-
+  let list = document.querySelector("#todo-selector");
   const form = document.querySelector("#form");
 
-   form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    // const input = document.querySelector("#input_todo");
-    const input = e.target.todo.value;
-    arr.push(input);
-  });
-  console.log(arr);
+  const renderUi = ()=>{
+    list.innerHTML = "";
+    
+    for(let i = 0;i<todos.length;i++){
+      const li = document.createElement("li");
+      li.textContent = todos[i].text + " ";
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "delete";
+       deleteButton.dataset.id = todos[i].id;
+      
+      deleteButton.setAttribute("class","delete");
+      li.appendChild(deleteButton);
+      list.appendChild(li);
+      console.log("rendered");
+    }
+  };
 
-  for (let i = 0; i < arr.length; i++) {
-    let li = document.createElement("li");
-    li.textContent = arr[i];
-    a.appendChild(li);
-  }
+  list.addEventListener("click",(e)=>{
+    if(e.target.tagName !== "BUTTON") return;
+    
+    const idToDelete = Number(e.target.dataset.id);
+    todos = todos.filter((todo) => todo.id !== idToDelete);
+    console.log(todos);
+    renderUi();
+  })
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const input = e.target.todo.value.trim();
+    
+    todos.push({id:j++,text:input});
+
+     renderUi();
+
+    e.target.reset();
+    //console.log(todos);
+  });
+
 };
 main();
