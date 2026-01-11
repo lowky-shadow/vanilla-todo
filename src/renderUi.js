fc3910b137f651882ec0b8920bef573f5795f645
lowky-shadow <share99anyone2@gmail.com>
2026-01-11 13:33:08 +0530

    feat: validation + accessibility

--- src/renderUi.js
+++ src/renderUi.js
@@ -8,6 +8,10 @@ export const renderUi = (todos, list) => {
       const inputbox = document.createElement("input");
       inputbox.dataset.id = todos[i].id;
       inputbox.value = todos[i].text;
+
+      inputbox.setAttribute("aria-label", "Edit todo text");
+      inputbox.setAttribute("aria-live", "polite");
+
       li.appendChild(inputbox);
       setTimeout(() => {
         inputbox.focus();
@@ -19,12 +23,15 @@ export const renderUi = (todos, list) => {
       const deleteButton = document.createElement("button");
       deleteButton.textContent = "delete";
       deleteButton.setAttribute("class", "delete");
+      deleteButton.setAttribute("aria-label", `Delete todo: ${todos[i].text}`);
+
       deleteButton.dataset.id = todos[i].id;
 
       const editButton = document.createElement("button");
       editButton.textContent = "edit";
       editButton.dataset.id = todos[i].id;
       editButton.setAttribute("class", "edit");
+      editButton.setAttribute("aria-label", `Edit todo: ${todos[i].text}`);
 
       li.appendChild(deleteButton);
       li.appendChild(editButton);
